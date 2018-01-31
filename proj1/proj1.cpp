764748788bc3d84ec1933ffaa61363e400a62796
Joshua Oliver Potter <jpotter@linprog3.cs.fsu.edu>
2018-01-30 21:14:15 -0500

    Fixing newline

--- proj1/proj1.cpp
+++ proj1/proj1.cpp
@@ -1,56 +1,20 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include <map>
 
 using namespace std;
 
-void insertCharacter(map<char,int> &characters, char character) {
-// 	cout << "Character: " << character << endl;
-	map<char,int>::iterator itr;
-	itr = characters.find(character);
-	if(itr == characters.end()) {
-		// add to characters map if not in vector
-		characters.insert({character, 1});
-	} else {
-		// if character already exists in vector, update frequency
-		itr->second = (itr->second + 1);
-	}
-}
+void insertCharacter(map<char,int> &characters, char character);
 
-void insertNumber(map<string,int> &numbers, string &number) {
-// 	cout << "Number: " << number << endl;
-	map<string,int>::iterator itr;
-	itr = numbers.find(number);
-	if(itr == numbers.end()) {
-		// add to characters map if not in vector
-		numbers.insert({number, 1});
-	} else {
-		// if character already exists in vector, update frequency
-		itr->second = (itr->second + 1);
-	}
-	number.clear();
-}
+void insertNumber(map<string,int> &numbers, string &number);
 
-void insertWord(map<string,int> &words, string &word) {
-// 	cout << "Word: " << word << endl;
-	map<string,int>::iterator itr;
-	itr = words.find(word);
-	if(itr == words.end()) {
-		// add to characters map if not in vector
-		words.insert({word, 1});
-	} else {
-		// if character already exists in vector, update frequency
-		itr->second = (itr->second + 1);
-	}
-	word.clear();
-}
+void insertWord(map<string,int> &words, string &word);
 
 int main() {
-	string input;
-	string word;
 	char prevChar = '\0';
 	int charNum = 1;
-	
+	string input;
+	string word;
 	map<string,int> words;
 	map<string,int> numbers;
 	map<char,int> characters;
@@ -58,6 +22,7 @@ int main() {
 	// get user input
 	while(getline(cin,input)) {
 		// get character from input unless no characters left to retrieve
+		// add to characters map
 		for (char c : input) {
 			// store character in lowercase for comparisons
 			char lower = tolower(c);
@@ -73,9 +38,6 @@ int main() {
 						// put word in words map
 						insertWord(words, word);
 					}
-					
-					// add character to characters map
-					insertCharacter(characters, c);
 					// add letter to temporary word storage
 					word.push_back(c);
 				} 
@@ -87,9 +49,6 @@ int main() {
 						// put number in numbers map
 						insertNumber(numbers, word);
 					}
-					
-					// add character to character map
-					insertCharacter(characters, c);
 					// add letter to temporary word storage
 					word.push_back(lower);
 				} 
@@ -128,10 +87,6 @@ int main() {
 							}
 						}
 					}
-					// add character to characters map
-					if (c >= ' ') {
-						insertCharacter(characters, c);
-					}
 				}
 			} else {
 				if(c >= '0' && c <= '9') {
@@ -141,13 +96,12 @@ int main() {
 					// push to word stack
 					word.push_back(lower);
 				}
-				
-				// add to characters map
-				insertCharacter(characters, c);
 			}
+			// add to characters map
+			insertCharacter(characters, c);
 			
 			// iterate number of character
-			charNum++;
+			++charNum;
 			prevChar = lower;
 			
 // 			if(charNum % 150000 == 0) {
@@ -161,33 +115,105 @@ int main() {
 		} else if (prevChar >= 'a' && prevChar <= 'z') {
 			insertWord(words, word);
 		}
+		
+		// add newline
+		if(cin.peek() != EOF) {
+			insertCharacter(characters, '\n');
+		}
 	}
 	
+	// print characters
 	if (characters.size() > 0) {
-		cout << "Characters" << endl;
-		characters.erase('\n');
+		cout << "Total " << characters.size() << " characters:" << endl;
 		for(auto result : characters) {
-			cout << "'" << result.first << "' => " << result.second << endl;
+			// format escaped characters
+			if (result.first == '\\') {
+				cout << "'\\' => " << result.second << endl;
+			} else if (result.first == '\'') {
+				cout << "'\'' => " << result.second << endl;
+			} else if (result.first == '\"') {
+				cout << "'\"' => " << result.second << endl;
+			} else if (result.first == '\t') {
+				cout << "'\\t' => " << result.second << endl;
+			} else if (result.first == '\v') {
+				cout << "'\\v' => " << result.second << endl;
+			} else if (result.first == '\r') {
+				cout << "'\\r' => " << result.second << endl;
+			} else if (result.first == '\n') {
+				cout << "'\\n' => " << result.second << endl;
+			} else if (result.first == '\0') {
+				cout << "'\\0' => " << result.second << endl;
+			}	else {
+				cout << "'" << result.first << "' => " << result.second << endl;
+			}
 		}
 		cout << endl;
 	}
 	
+	// print numbers
 	if (numbers.size() > 0) {
-		cout << "Numbers" << endl;
 		numbers.erase("");
+		cout << "Total " << numbers.size() << " numbers:" << endl;
+// 		cout << "Debug: " << int(numbers.begin()->first[0]) << " => " << numbers.begin()->second << endl;
 		for(auto result : numbers) {
 			cout << result.first << " => " << result.second << endl;
 		}
 		cout << endl;
 	}
 	
+	// print words
 	if (words.size() > 0) {
-		cout << "Words" << endl;
 		words.erase("");
+		cout << "Total " << words.size() << " words:" << endl;
+// 		cout << "Debug: " << int(words.begin()->first[0]) << " => " << words.begin()->second << endl;
 		for(auto result : words) {
 			cout << result.first << " => " << result.second << endl;
 		}
 	}
 	
 	return 0;
+}
+
+void insertCharacter(map<char,int> &characters, char character) {
+	// insert character to characters map
+	
+	map<char,int>::iterator itr;
+	itr = characters.find(character);
+	if(itr == characters.end()) {
+		// add to characters map if not in map
+		characters.insert({character, 1});
+	} else {
+		// if character already exists in map, update frequency
+		itr->second = (itr->second + 1);
+	}
+}
+
+void insertNumber(map<string,int> &numbers, string &number) {
+	// insert number to numbers map
+	
+	map<string,int>::iterator itr;
+	itr = numbers.find(number);
+	if(itr == numbers.end()) {
+		// add to number map if not in map
+		numbers.insert({number, 1});
+	} else {
+		// if number already exists in map, update frequency
+		itr->second = (itr->second + 1);
+	}
+	number.clear();
+}
+
+void insertWord(map<string,int> &words, string &word) {
+	// insert word to words map
+	
+	map<string,int>::iterator itr;
+	itr = words.find(word);
+	if(itr == words.end()) {
+		// add to words map if not in map
+		words.insert({word, 1});
+	} else {
+		// if word already exists in map, update frequency
+		itr->second = (itr->second + 1);
+	}
+	word.clear();
 }
