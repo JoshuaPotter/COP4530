0b88dcbda2ebcc9e264bbd4e71f86db440f1dd0b
Joshua Oliver Potter <jpotter@linprog3.cs.fsu.edu>
2018-02-12 22:25:52 -0500

    Added comments

--- proj2/List.hpp
+++ proj2/List.hpp
@@ -1,20 +1,36 @@
+/*
+  Name: Joshua Potter (jop13)
+  Class: COP4530
+  Assignment 2 - List.hpp
+*/
+
 #include "List.h"
 
-// nested const_iterator class definitions
+/*
+   
+   Nested const_iterator class definitions
+
+*/
+
+// initialization
 template <typename T>
 List<T>::const_iterator::const_iterator() : current { nullptr } { }
 
+
+// const * operator overload
 template <typename T>
 const T & List<T>::const_iterator::operator*() const { 
   return retrieve();
 }
 
+// post-increment overload
 template <typename T>
 typename List<T>::const_iterator & List<T>::const_iterator::operator++() { 
   current = current->next;
   return *this;
 }
 
+// pre-increment overload
 template <typename T>
 typename List<T>::const_iterator List<T>::const_iterator::operator++(int) { 
   List<T>::const_iterator old = *this;
@@ -22,12 +38,14 @@ typename List<T>::const_iterator List<T>::const_iterator::operator++(int) {
   return old;
 }
 
+// post-decrement overload
 template <typename T>
 typename List<T>::const_iterator & List<T>::const_iterator::operator--() { 
   current = current->prev;
   return *this;
 }
 
+// pre-decrement overload
 template <typename T>
 typename List<T>::const_iterator List<T>::const_iterator::operator--(int) { 
   List<T>::const_iterator old = *this;
@@ -35,44 +53,58 @@ typename List<T>::const_iterator List<T>::const_iterator::operator--(int) {
   return old;
 }
 
+// equals to comparison operator overload
 template <typename T>
 bool List<T>::const_iterator::operator==(const List<T>::const_iterator & rhs) const { 
   return current == rhs.current;
 }
 
+// not equals to comparison operator overload
 template <typename T>
 bool List<T>::const_iterator::operator!=(const List<T>::const_iterator & rhs) const { 
   return !(*this == rhs);
 }
 
+// turn data value of node
 template <typename T>
 T & List<T>::const_iterator::retrieve() const {
   return current->data;
 }
 
+// go to node at position p
 template <typename T>
 List<T>::const_iterator::const_iterator(Node *p) : current { p } { }
 
-// nested iterator class definitions
+/*
+  
+  Nested iterator class definitions
+
+*/
+
+// initialization
 template <typename T>
 List<T>::iterator::iterator() { }
 
+// * operator overload
 template <typename T>
 T & List<T>::iterator::operator*() {
   return List<T>::const_iterator::retrieve();
 }
 
+// const * operator overload
 template <typename T>
 const T & List<T>::iterator::operator*() const {
   return List<T>::const_iterator::operator*();
 }
 
+// pre-increment overload
 template <typename T>
 typename List<T>::iterator & List<T>::iterator::operator++() {
   this->current = this->current->next;
   return *this;
 }
 
+// post-increment overload
 template <typename T>
 typename List<T>::iterator List<T>::iterator::operator++(int) {
   List<T>::iterator old = *this;
@@ -80,12 +112,14 @@ typename List<T>::iterator List<T>::iterator::operator++(int) {
   return old;
 }
 
+// pre-decrement overload
 template <typename T>
 typename List<T>::iterator & List<T>::iterator::operator--() {
   this->current = this->current->prev;
   return *this;
 }
 
+// post-decrement overload
 template <typename T>
 typename List<T>::iterator List<T>::iterator::operator--(int) {
   List<T>::iterator old = *this;
