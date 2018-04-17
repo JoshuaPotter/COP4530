20e5a95d5239c8a722a53c657a544228a0833dcb
Joshua Oliver Potter <jpotter@linprog3.cs.fsu.edu>
2018-04-17 18:53:25 -0400

    Hash table progress

---
+++ proj5/hashtable.h
@@ -0,0 +1,56 @@
+/*
+  Name: Joshua Potter (jop13)
+  Class: COP4530
+  Assignment 5 - hashtable.h
+*/
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include <iostream>
+#include <vector>
+#include <list>
+#include <function>   // std::hash
+#include <iterator>   // std::begin, std::end
+#include <algorithm>  // std::find
+#include <utility>    // std::pair
+
+namespace cop4530 {
+  // max_prime is used by the helpful functions provided
+  // to you.
+  static const unsigned int max_prime = 1301081;
+  // the default_capacity is used if the initial capacity 
+  // of the underlying vector of the hash table is zero. 
+  static const unsigned int default_capacity = 11;
+  
+  template typename K, typename V>
+  class HashTable {
+    public:
+      HashTable(size_t size); // constructor. Create a hash table, where the size of the vector is set to prime_below(size) (where size is default  to 101), where prime_below() is a private member function of the HashTable and provided to you.
+      ~HashTable(); // destructor. Delete all elements in hash table.
+      bool contains(const K & k); // check if key k is in the hash table.
+      bool match(const std::pair<K, V> &kv); const // check if key-value pair is in the hash table.
+      bool insert(const std::pair<K, V> & kv);  // add  the key-value pair kv into the hash table. Don't add if kv is already in the hash table. If the key is the hash table but with a different value, the value should be updated to the new one with kv. Return true if kv is inserted or the value is updated; return false otherwise (i.e., if kv is in the hash table).
+      bool insert (std::pair<K,  V> && kv); // move version of insert.
+      bool remove(const K & k); // delete the key k and the corresponding value if it is in the hash table. Return true if k is deleted, return false otherwise (i.e., if key k is not in the hash table).
+      void clear(); // delete all elements in the hash table
+      bool load(const char *filename); // load the content of the file with name filename into the hash table. In the file, each line contains a single pair of key and value, separated by a white space.
+      void dump(); // display all entries in the hash table. If an entry contains multiple key-value pairs, separate them by a semicolon character (:) (see the provided executable for the exact output format).
+      size_t size(); // return the number of elements in the hash table.
+      bool write_to_file(const char *filename); // write all elements in the hash table into a file with name filename. Similar to the file format in the load function, each line contains a pair of key-value pair, separated by a white space.
+        
+    private:
+      void makeEmpty(); // delete all elements in the hash table. The public interface clear() will call this function.
+      void rehash(); // Rehash function. Called when the number of elements in the hash table is greater than the size of the vector.
+      size_t myhash(const K &k); // return the index of the vector entry where k should be stored.
+      unsigned long prime_below (unsigned long); // determine the new proper underlying vector size
+      void setPrimes(vector<unsigned long>&); // set vector size
+    
+      // member data
+      int currentSize;
+      std::vector<std::list<std::pair<K,V>>> theLists; // vector list of KEY, VALUE pairs
+  }
+  
+  #include "hashtable.hpp"
+}
+ 
+#endif
---
+++ proj5/hashtable.hpp
@@ -0,0 +1,182 @@
+/*
+  Name: Joshua Potter (jop13)
+  Class: COP4530
+  Assignment 5 - hashtable.hpp
+*/
+
+#include "hashtable.h"
+
+// constructor
+template <typename K, typename V>
+HashTable<K, V>::HashTable(size_t size = 101) {
+	currentSize = 0;
+	theLists.resize(prime_below(size));
+}
+
+template <typename K, typename V>
+HashTable<K, V>::~HashTable() {
+	clear();
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::contains(const K & k) {
+	auto & whichList = theLists[myhash(x)];
+	return find(begin(whichList), end(whichList), x) != end(whichList);
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::match(const std::pair<K, V> &kv) const {
+	
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::insert(std::pair<K,  V> && kv) {
+	auto & whichList = theLists[myhash(x)];
+	if(find(begin(whichList), end(whichList), x) != end(whichList)) {
+		return false;
+	}
+	whishList.push_back(x);
+	
+	// rehash
+	if(++currentSize > theLists.size()) {
+		rehash();
+	}
+	
+	return true;
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::remove(const K & k) {
+	auto & whichList = theLists[myhash(x)];
+	auto itr = find(begin(whichList), end(whichList), x);
+	
+	if(itr == end(whichList)) {
+		return false;
+	}
+	
+	whichList.erase(itr);
+	--currentSize;
+	return true;
+}
+
+template <typename K, typename V>
+void HashTable<K, V>::clear() {
+	makeEmpty();
+}
+
+template <typename K, typename V>
+void HashTable<K, V>::load(const char *filename) {
+	
+}
+
+template <typename K, typename V>
+void HashTable<K, V>::dump() {
+	
+}
+
+template <typename K, typename V>
+size_t HashTable<K, V>::size() {
+	return currentSize;
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::write_to_file(const char *filename) {
+	
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::makeEmpty() {
+	for(auto & thisList : theLists) {
+		thisList.clear();
+	}
+}
+
+template <typename K, typename V>
+bool HashTable<K, V>::rehash() {
+	std::vector<std::list<std::pair<K, V>> > oldLists = theLists;
+	
+	// create new list with double the size
+	theLists.resize(prime_below(2 * theLists.size()));
+	for(auto & thisList : theLists) {
+		thisList.clear();
+	}
+	
+	// copy over new list
+	currentSize = 0;
+	for(auto & thisList : oldLists) {
+		for(auto & x : thisList) {
+			insert(std::move(x));
+		}
+	}
+}
+
+template <typename K, typename V>
+size_t HashTable<K, V>::myhash(const K &k) {
+//  book implementation
+	
+// 	size_t hashVal = 0;
+	
+// 	for(char ch : key) {
+// 		hashVal = 37 * hashVal + ch;
+// 	}
+	
+// 	return hashVal % theLists.size();
+	
+	static std::hash<K> hf;
+	return hf(x) % theLists.size();
+}
+
+// returns largest prime number <= n or zero if input is too large
+// This is likely to be more efficient than prime_above(), because
+// it only needs a vector of size n
+template <typename K, typename V>
+unsigned long HashTable<K, V>::prime_below (unsigned long n) {
+  if (n > max_prime)
+    {
+      std::cerr << "** input too large for prime_below()\n";
+      return 0;
+    }
+  if (n == max_prime)
+    {
+      return max_prime;
+    }
+  if (n <= 1)
+    {
+		std::cerr << "** input too small \n";
+      return 0;
+    }
+
+  // now: 2 <= n < max_prime
+  std::vector <unsigned long> v (n+1);
+  setPrimes(v);
+  while (n > 2)
+    {
+      if (v[n] == 1)
+	return n;
+      --n;
+    }
+
+  return 2;
+}
+
+//Sets all prime number indexes to 1. Called by method prime_below(n) 
+template <typename K, typename V>
+void HashTable<K, V>::setPrimes(std::vector<unsigned long>& vprimes) {
+  int i = 0;
+  int j = 0;
+
+  vprimes[0] = 0;
+  vprimes[1] = 0;
+  int n = vprimes.capacity();
+
+  for (i = 2; i < n; ++i)
+    vprimes[i] = 1;
+
+  for( i = 2; i*i < n; ++i)
+    {
+      if (vprimes[i] == 1)
+        for(j = i + i ; j < n; j += i)
+          vprimes[j] = 0;
+    }
+}
+
